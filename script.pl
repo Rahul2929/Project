67fad726eaca2512cf72a700134506181f85f0e8
Rahul2929 <Rahul2929@hotmail.com>
2017-05-31 17:22:33 +0530

    Update script.pl

--- script.pl
+++ script.pl
@@ -1,1 +1,23 @@
 #!/usr/bin/perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Maths;
+
+my $obj = new Maths(20,60);
+$obj->sum();
+print $obj->{result};
+print "\n";
+
+$obj->sub();
+print $obj->{result};
+print "\n";
+
+$obj->div();
+print $obj->{result};
+print "\n";
+
+$obj->multiplication();
+print $obj->{result};
+print "\n";
